8b45646fca6128f69df8fb76446165064da51334
Sylvain T <garvek@gmail.com>
2024-06-20 19:34:00 +0200

    Default page

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bulma';
+import 'fontawesome';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
